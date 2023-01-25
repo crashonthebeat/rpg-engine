c6614c8bddf3b76e1b13c2b64caf476c4e28c40d
crashonthebeat <64339619+crashonthebeat@users.noreply.github.com>
2023-01-24 18:09:13 -0800

    Added Backpack functionality
    Reworked Search for Item to allow searching for worn items, and
    searching containers that are equipped on the body.
    
    Also moved the wield/unwield and wear/unwear into equip/unequip with
    different parts of the function going to different entity classes.

--- src/player.py
+++ src/player.py
@@ -1,4 +1,4 @@
-import os
+import os, itertools
 
 from time import sleep
 from src.basicTypes import Container, Person
@@ -25,17 +25,27 @@ class Player(Person, Container):
         self.inventory = {}
         self.container_inventory = []
         self.items_worn = []
+        self.items_held = []
+        self.helditems_d = ['You are currently holding:']
+        self.wornitems_d = ['You are currently wearing:']
         self.can_close = False
         self.open = True
 
     def describe(self):
-        super(Person, self).describe()
-        print("You are currently wearing:")
-        if len(self.items_worn) > 0:
-            for article in self.items_worn:
-                print(f"{article.name.capitalize()} on your {article.primary_slot}.")
-        else:
-            print("Nothing.")
+        # This is the method that will display the self description, as well
+        # as print the inventory and held items.
+
+        # The two lines below 
+        cols = [self.wornitems_d, self.helditems_d]
+        items_table = self.make_table(cols)
+
+        super(Container, self).describe()
+        print(items_table)
+
+        # These two functions resets any added spaces in the items table.
+        # Otherwise there's a leapfrogging effect for the columns
+        self.wornitems_d = [i for i in self.wornitems_d if i != ' ']
+        self.helditems_d = [i for i in self.helditems_d if i != ' ']
 
     def travel(self, action, direction):
         # This is the travel method used to move the player between
@@ -59,12 +69,53 @@ class Player(Person, Container):
             self.current_room = new_room
         else:
             print(f"You cannot go {direction}.")
+    
+    def search_for_box(self, box, action):
+        # This is the method to find a subcontainer in a given container.
+        
+        box_id = self.current_room.find_box(box, self.container_inventory)
+        if box_id == False:  # If the user box isn't found, stop function.
+                print(f"You don't see the {box} {self.current_room.list_desc.lower()}.")
+        elif box_id == 'multiple':  # If search returned multiple boxes.
+            print(f"Which {box} do you want to {action}?")
+            box_id = False
+            # The user will need to be more specific.
+        elif box_id.is_container == False:
+            print(f"{box_id.name.capitalize()} is not a container.")
+        else: pass
+
+        return box_id
+
+    def find_equipped_item(self, search_item):
+        for item in itertools.chain(self.items_held, self.items_worn):
+            if search_item in item.name:
+                return item, self
+        else: return False, self
+
+    def search_for_item(self, item, parent_box, action):
+        # This searches a found container for an item given by user input
+        # and returns the item_id as well as the id of any subcontainer it
+        # was found in. 
+        
+        item_id, box_id = parent_box.find_item(item)
+        if parent_box == self and item_id == False:
+            item_id, box_id = self.find_equipped_item(item)
+        if parent_box == self and item_id == False:    
+            self.current_room.find_item(item)
+
+        if item_id == False:  # If the item isn't found, stop function.
+            print(f"You don't see the {item} {parent_box.list_desc.lower()}.")
+            return False, parent_box
+        elif item_id == 'multiple':  # If there are multiple matches.
+            print(f"Which {item} do you want to {action}?")
+            return False, parent_box
+        else: return item_id, box_id
 
     def look(self, obj, ind_obj):
         # This calls the describe method for an entity if the entity exists.
 
         # If player types 'look at object': 
-        if ind_obj: obj = ind_obj
+        if obj == False: obj = ind_obj
         else: pass
         
         # If player types 'look room' or 'look'
@@ -74,35 +125,15 @@ class Player(Person, Container):
         elif obj == 'self':
             self.describe()
             return True
-        else: 
-            obj_id, parend_id = self.current_room.find_item(obj)
-            if obj_id == False:
-                obj_id, parent_id = self.current_room.find_box(obj)
+        else:  # If player specified a non-self object:
+            obj_id, parent_id = self.search_for_item(
+                obj, self, 'look at')
+            # Search for items that match.
 
-        if obj_id == 'multiple': 
-            print(f"Which {obj} do you want to look at?")
-        elif obj_id: 
-            obj_id.describe()
-        else: 
-            print(f"You can't find {obj}.")
-
-    
-    def search_for_box(self, box, parent_box):
-        # This is the method to find a subcontainer in a given container.
-        
-        box_id = parent_box.find_box(box)
-        if box_id == False:  # If the user box isn't found, stop function.
-                print(f"You don't see the {box} here.")
-        elif box_id == 'multiple':  # If search returned multiple boxes.
-            print(f"Which {box} do you mean?")
-            box_id = False
-            # The user will need to be more specific.
-        else: pass
-
-        return box_id
+        if obj_id: obj_id.describe()
 
     def open_box(self, box):
-        box_id = self.search_for_box(box, self.current_room)
+        box_id = self.search_for_box(box, "open")
         
         if box_id == 'multiple': 
             pass
@@ -112,25 +143,9 @@ class Player(Person, Container):
             box_id.open_self()
 
     def close_box(self, box):
-        box_id = self.search_for_box(box, self.current_room)
+        box_id = self.search_for_box(box, "close")
 
         if box_id: box_id.close_self()
-
-    
-    def search_for_item(self, item, parent_box):
-        # This searches a found container for an item given by user input
-        # and returns the item_id as well as the id of any subcontainer it
-        # was found in. 
-
-        item_id, box_id = parent_box.find_item(item)
-
-        if item_id == False:  # If the item isn't found, stop function.
-            print(f"You don't see the {item} here.")
-            return False, parent_box
-        elif item_id == 'multiple':  # If there are multiple matches.
-            print(f"Which {item} do you mean?")
-            return False, parent_box
-        else: return item_id, box_id
 
 
     def get_item(self, item, box):
@@ -140,7 +155,7 @@ class Player(Person, Container):
 
         # If the user gives a specific container, search for that first.
         if box: 
-            box_id = self.search_for_box(box, self.current_room)
+            box_id = self.search_for_box(box, "get")
             if box_id == False: return True  # If no box is found, exit.
             elif box_id.open == False: 
                 print(f"You need to open {box_id.name} first.")
@@ -148,46 +163,49 @@ class Player(Person, Container):
         else: 
             box_id = self.current_room  # If no container is given.
 
-        item_id, box_id = self.search_for_item(item, box_id)
+        item_id, box_id = self.search_for_item(item, box_id, "get")
 
         # If the item was found (and box if given), then add and remove the items.
         if item_id:
             box_id.remove_item(item_id)
             self.add_item(item_id)
+            if item_id.is_container: 
+                self.container_inventory.append(item_id)
             print(f"You get {item_id.name} from {box_id.name}!")
-        else: pass
 
 
     def drop_item(self, item):
         # This method will find an item in the player's inventory and "drops" 
         # it into the room's inventory.
 
-        item_id, box = self.search_for_item(item, self)
+        item_id, parent_id = self.search_for_item(item, self, "drop")
 
         if item_id:
             self.remove_item(item_id)
+            if item_id.is_container: self.container_inventory.remove(item_id)
             self.current_room.add_item(item_id)
-            print(f"You drop {item.name}")
+            print(f"You drop {item_id.name}")
         else: pass
 
 
-    def place_item(self, item, box):
+    def place_item(self, item, box, prep):
         # This method takes an item from the player's inventory and places it
         # into a given container.
 
-        # First try to find the box, as in the methods above.
-        box_id = self.search_for_box(box, self.current_room)
+        box_id = self.search_for_box(box, "place into")
+
         if not box_id: return True
         elif box_id.open == False: 
             print(f"You need to open {box_id.name} first.")
             return True
         else: pass
 
-        item_id, parent_id = self.search_for_item(item, self)
+        item_id, parent_id = self.search_for_item(item, self, "place")
         if item_id:
             self.remove_item(item_id)
+            if item_id.is_container: self.container_inventory.remove(item_id)
             box_id.add_item(item_id)
-            print(f"You place {item_id.name} on {box_id.name.lower()}.")
+            print(f"You place {item_id.name} {prep} {box_id.name.lower()}.")
         else: pass
 
 
@@ -203,41 +221,72 @@ class Player(Person, Container):
         else: return True
             
 
-    def wear_item(self, item):
+    def equip_item(self, item, action):
         # This method takes an item from the player's inventory and
         # adds it to a slot.
 
-        item_id, box_id = self.search_for_item(item, self)
+        item_id, box_id = self.search_for_item(item, self, action)
 
-        if not item_id:  # If there's no item on your body
-            print(f"You don't have {item}")
+        if not item_id:  # If there's no item in your inventory.
             return True
-        elif item_id.entityType != "wearable":
-            print("You can't wear that!")
-            return True
-        else: pass
 
-        if self.test_for_fit(item_id):
-            for slot in item_id.used_slots.keys():
-                self.wearable_slots[slot] -= item_id.used_slots[slot]
+        # If the item is clothing or armor: do the following    
+        elif item_id.entityType == "wearable":
+            if self.test_for_fit(item_id):  # Test if item will fit
+                # Subtracting the item's used slots from the pc's available slots
+                for slot in item_id.used_slots.keys():
+                    self.wearable_slots[slot] -= item_id.used_slots[slot]
             
-            print(f"You put on {item_id.name}")
-            self.items_worn.append(item_id)
-            self.remove_item(item_id)
+                print(f"You put on {item_id.name}")
+                self.items_worn.append(item_id)  # Add to worn items
+                self.wornitems_d.append(item_id.wear_d)
+                # Add string to self description for worn items
+                if item_id.is_container: item_id.equip_bag()
+                # Allow self-search to find items in container if it is.
+                self.remove_item(item_id)
 
-    def unwear_item(self, item):
+        # If the item is a tool or weapon: do the following
+        elif item_id.entityType == "wieldable":
+            if item_id.hand_slots > self.free_hands:
+                print(f"You need {item_id.hand_slots} free hands for this.")
+            else:
+                self.free_hands -= item_id.hand_slots  # Remove hand slots
+                self.remove_item(item_id)  # Remove from inventory
+                self.items_held.append(item_id)  # Add to held items
+                self.helditems_d.append(item_id.hold_d)
+                # Add string to self-description
+                print(f"You {action} {item_id.name}.")
+        else: print(f"You can't {action} that!")
+        
+
+    def unequip_item(self, item):
+        # This method removes an article of clothing or armor from
+        # the pc's worn/held items slots and places it back in their
+        # inventory.
+
         item_id = False  # This might come back to bite me later.
 
-        for article in self.items_worn:
-            if item in article.name:
-                item_id = article
+        for article in itertools.chain(self.items_worn, self.items_held):
+            # The iterating find loop to translate the input string
+            # into the actual item.
+            if item in article.name: item_id = article
 
-        if item_id == False: return True
+        if not item_id: pass
+        elif item_id.entityType == 'wearable':
+            print(f"You remove {item_id.name}.")
+            for slot in item_id.used_slots.keys():
+                # Adding the item's used slots back into the available slots
+                self.wearable_slots[slot] += item_id.used_slots[slot]
+
+            if item_id.is_container: item_id.unequip_bag()  # close bag
+            self.items_worn.remove(item_id)  # Remove item from worn items
+            self.wornitems_d.remote(item_id.wear_d)  # Remove from desc
+            self.add_item(item_id)  # Add item back into inventory
+        elif item_id.entityType == 'wieldable':
+            self.free_hands += item_id.hand_slots  # add hand slots back
+
+            print(f"You put away {item_id.name}.")
+            self.items_held.remove(item_id)  # Remove from held items
+            self.helditems_d.remove(item_id.hold_d)  # Remove from desc
+            self.add_item(item_id)  # Add back to inventory
         else: pass
-        
-        for slot in item_id.used_slots.keys():
-            self.wearable_slots[slot] += item_id.used_slots[slot]
-
-        print(f"You remove {item_id.name}.")
-        self.items_worn.remove(item_id)
-        self.add_item(item_id)
