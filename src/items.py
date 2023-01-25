772544d3b289a347742236896babf316d911dc37
crashonthebeat <64339619+crashonthebeat@users.noreply.github.com>
2023-01-24 18:07:25 -0800

    Added a Bag as an Item Class.
    A bag is basically a container that can be worn. Eventually this
    will be expanded to pouches. I'm thinking that instead of making
    holsters a type of bag, I will make them their own separate type
    of Wearable that only accepts one type of item. For example, a
    sheathe can only accept a sword. I can expand that "holster" code
    to a weapon rack.

--- src/items.py
+++ src/items.py
@@ -1,4 +1,4 @@
-from src.basicTypes import Entity
+from src.basicTypes import Entity, Container
 
 class Item(Entity):
     # Basic item class, will be passed to all item types. Nothing is here
@@ -7,6 +7,7 @@ class Item(Entity):
         Entity.__init__(self, name)
         self.desc = desc
         self.entityType = "item"  # The coding name for an item.
+        self.is_container = False
 
 class Interactable(Item):
     # These are items that cannot be moved from their locations but can
@@ -16,24 +17,57 @@ class Interactable(Item):
         self.desc = desc
         self.entityType = "interactable"
 
-class Wieldable(Item):
-    # These are all the items that can be used or 'wielded'
-    def __init__(self, name, desc):
+class Tool(Item):
+    # These are all the items that can be used or 'wielded' incl weapons.
+    def __init__(self, name, desc, hand_slots):
         Entity.__init__(self, name)
         self.desc = desc
+        self.hand_slots = hand_slots  # How many hands does this item need?
         self.entityType = "wieldable"
-        self.hand_slots = 1  # How many hands does this item need to hold it?
+        self.hold_d = f"{self.name.capitalize()}"
 
-class Wearable(Item):
-    # These are all the things that can be worn on a person.
-    def __init__(self, name, desc):
+class Clothing(Item):
+    # These are all the things that can be worn on a person incl armor.
+    def __init__(self, name, desc, primary_slot):
         Entity.__init__(self, name)
         self.desc = desc
+        self.primary_slot = primary_slot
         self.entityType = "wearable"
         self.used_slots = {}  # What body slots the wearable uses.
-        self.primary_slot = False
+        self.wear_d = f"{self.name.capitalize()} on your {self.primary_slot}."
+        self.is_container = False
 
-class Clothing(Wearable):
-    # These are all the things that can be worn on a person.
-    def __init__(self, name, desc):
-        Wearable.__init__(self, name, desc)
+class Bag(Clothing, Container):
+    # This class is for all containers that can be worn.
+    def __init__(self, name, desc, primary_slot):
+        Clothing.__init__(self, name, desc, primary_slot)
+        self.desc = desc
+        self.primary_slot = primary_slot
+        self.entityType = "wearable"
+        self.is_container = True
+        self.open = False  # All wearable boxes are closed by default
+        self.can_close = True
+        self.inventory = {}
+        self.container_inventory = []
+
+    def equip_bag(self):
+        # These are the actions that happen when a bag is equipped
+        self.open = True
+        self.can_close = False
+
+    def unequip_bag(self):
+        # These happen when a bag is unequipped.
+        self.open = False
+        self.can_close = True
+
+# The following are classes I'm naming for future addition.
+
+# Not sure how combat is going to work yet, so I'm waiting until then to
+# build out these two classes.
+class Weapon(Tool):
+    def __init__(self, name, desc, hand_slots):
+        Tool.__init__(self, name, desc, hand_slots)
+
+class Armor(Clothing):
+    def __init__(self, name, desc, primary_slot):
+        Clothing.__init__(self, name, desc, primary_slot)
