77091b96b3149e40de8383e34990ce0b3f69f48c
crashonthebeat <64339619+crashonthebeat@users.noreply.github.com>
2023-01-24 17:58:05 -0800

    Cleaned up inits, added backpacks
    Had to play around with primary slots for items in order to get
    the self description look correct. Backpacks then just followed
    the rest of these templates.

--- src/clothing.py
+++ src/clothing.py
@@ -1,4 +1,4 @@
-from src.items import Clothing
+from src.items import Clothing, Bag
 
 # EXPLANATION OF SLOTS:
 # Any kind of undergarment takes half a slot.
@@ -16,155 +16,147 @@ from src.items import Clothing
 ################
 
 class Hat(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='head'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = { 'head': 1 }
-        self.primary_slot = "head"
 
 class Veil(Clothing):
     # Anything thin that covers the head and can go under a hat or helm
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='head'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = { 'head': 0.5 }
-        self.primary_slot = "head"
 
 class Eyewear(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='eyes'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = { 'eyes': 1 }
-        self.primary_slot = "eyes"
 
 ############
 ### TOPS ###
 ############
 
 class Jacket(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='torso'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = { 
             'l_should': 1, 'r_should': 1, 'l_arm': 1, 'r_arm': 1,
             'l_wrist': 1, 'r_wrist': 1, 'chest': 1, 'waist': 1
          }
-        self.primary_slot = "torso"
 
 class Shirt(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='torso'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = { 
             'l_should': 1, 'r_should': 1, 'l_arm': 1, 'r_arm': 1,
             'chest': 1, 'waist': 1
          }
-        self.primary_slot = "torso"
 
 class Tunic(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
-        self.used_slots = { 
+    def __init__(self, name, desc, primary_slot='torso'):
+        Clothing.__init__(self, name, desc, primary_slot)
+        self.used_slots = {
             'l_should': 1, 'r_should': 1, 'chest': 1, 'waist': 1
-         }
-        self.primary_slot = "torso"
+        }
 
 ###############
 ### BOTTOMS ###
 ###############
 
 class Trousers(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='legs'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = { 
             'hips': 1, 'l_thigh': 1, 'r_thigh': 1, 
             'l_shin': 0.5, "r_shin": 0.5
          }
-        self.primary_slot = "legs"
 
 class Skirt(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='legs'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = { 'hips': 1 }
-        self.primary_slot = "legs"
 
 ################
 ### FOOTWEAR ###
 ################
 
 class Boots(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='feet'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = {
             'l_foot': 1, 'r_foot': 1,
             'l_shin': 1, 'r_shin': 1,
         }
-        self.primary_slot = "feet"
 
 class Shoes(Clothing):
     # Can be sandals also
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='feet'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = {
             'l_foot': 1, 'r_foot': 1,
         }
-        self.primary_slot = "feet"
 
 #################
 ### FULL BODY ###
 #################
 
 class Dress(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='body'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = {
             'l_should': 1, 'r_should': 1, 'l_arm': 1, 'r_arm': 1,
             'chest': 1, 'waist': 1, 'hips': 0.5
         }
-        self.primary_slot = "body"
 
 class Robe(Clothing):
     # A loose dress basically that's tighter on the hips (cinching)
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='body'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = { 
             'l_should': 1.5, 'r_should': 1.5, 'l_arm': 0.5, 'r_arm': 0.5,
             'l_wrist': 0.5, 'r_wrist': 0.5, 'chest': 0.5, 
             'waist': 0.5, 'hips': 1.5
         }
-        self.primary_slot = "body"
+
+##################
+### BACK ITEMS ###
+##################
+
+class Cape(Clothing):
+    def __init__(self, name, desc, primary_slot='back'):
+        Clothing.__init__(self, name, desc, primary_slot)
+        self.used_slots = {'back': 1}
+
+class Backpack(Bag):
+    def __init__(self, name, desc, primary_slot='back'):
+        Bag.__init__(self, name, desc, primary_slot)
+        self.used_slots = {'back': 1}
+        self.list_desc = 'in this backpack'
 
 ###################
 ### ACCESSORIES ###
 ###################
 
 class Gloves(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='hands'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = {'l_hand': 1, 'r_hand': 1}
-        self.primary_slot = "hands"
 
 class Amulet(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='neck'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = {'neck': 1.5}
-        self.primary_slot = "neck"
 
 class Choker(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='neck'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = {'neck': 0.5}
-        self.primary_slot = "neck"
 
 class Ring(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='finger'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = {'fingers': 1}
-        self.primary_slot = "finger"
-
-class Cape(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
-        self.used_slots = {'back': 1}
-        self.primary_slot = "back"
 
 class wornShield(Clothing):
-    def __init__(self, name, desc):
-        Clothing.__init__(self, name, desc)
+    def __init__(self, name, desc, primary_slot='back'):
+        Clothing.__init__(self, name, desc, primary_slot)
         self.used_slots = {'back': 1}
-        self.primary_slot = "back"
